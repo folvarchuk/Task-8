54b0c766da600bb4f50e1d51b145c073354e9bda
andrey <andrey@0D6C>
2021-12-12 17:34:32 +0300

    Добавьте файлы проекта.

---
+++ Задание 8_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Выберите любую папку на своем компьютере, имеющую вложенные директории. Выведите на консоль ее содержимое и содержимое всех подкаталогов.*/
+
+            DirectoryInfo directory = new DirectoryInfo("Test_8_1");
+            foreach(DirectoryInfo firstFolder in directory.GetDirectories())
+            {
+                Console.WriteLine(firstFolder.Name);
+                foreach(DirectoryInfo otherFolders in firstFolder.GetDirectories())
+                {
+                    Console.WriteLine(otherFolders.Name);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание 8_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Программно создайте текстовый файл и запишите в него 10 случайных чисел. Затем программно откройте созданный файл, рассчитайте сумму чисел в нем, ответ выведите на консоль.*/
+
+            string path = "Task_8-2.txt";
+            int[] array = new int[10];
+            Random random = new Random();
+            int S = 0;
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    array[i] = random.Next(0, 10);
+                    sw.WriteLine(array[i]);
+                }
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                foreach (int i in array)
+                {
+                    int x = Convert.ToInt32(sr.ReadLine());
+                    S += x;
+                }
+                Console.WriteLine(S);
+            }
+            Console.ReadKey();
+        }
+    }
+}
